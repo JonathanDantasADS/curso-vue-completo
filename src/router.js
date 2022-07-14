533fed41cc1313a3efc1b9c669a8a27b67004fbb
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-07-13 21:47:57 -0300

    configuração ... vue router

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+
+const routes = [
+    {
+        path: '/home', // localhost:8080/home
+        component: Home
+    },
+    {
+        path: '/login', // localhost:8080/login
+        component: Login
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
