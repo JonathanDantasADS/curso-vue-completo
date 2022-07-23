03683d43edb986de7515cb16ac7a624b8186b4e7
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-07-23 14:20:44 -0300

    Query Params via navegação programática FORMULARIO [Importante]

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <h5>Contratos</h5>
-        <router-link class="btn btn-primary" :to="{ name: 'contratos', query: { leadId_like: 1} }">LeadId = 1</router-link>
-        <router-link class="btn btn-primary" to="/home/vendas/contratos?servicoId_like=2">ServicoId = 2</router-link>
 
-        <router-link class="btn btn-primary" :to="{ name: 'contratos', query: { leadId_like: 1, servicoId_like: 2} }">LeadId = 1 e servico_Id = 2</router-link>
-        <router-link class="btn btn-primary" to="/home/vendas/contratos?servicoId_like=2&leadId_like=2">ServicoId = 2 e leadId = 2</router-link>
-        
+    <div class="card mb-4">
+        <div class="card-header">Contratos</div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <label class="form-label">ID Contrato:</label>
+                    <input type="text" class="form-control" v-model="formPesquisa.id_like">
+                </div>
+                <div class="col-6">
+                    <label class="form-label">Data início:</label>
+                    <div class="input-group">
+                        <input type="date" class="form-control" v-model="formPesquisa.data_inicio_gte">
+                        <input type="date" class="form-control" v-model="formPesquisa.data_inicio_lte">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-primary" @click="pesquisar">Pesquisar</button>
+        </div>
+    </div>
+
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -44,10 +60,29 @@ import ApiMixin from '@/mixins/ApiMixin'
         name: 'Leads',
         mixins: [ApiMixin],
         data: () => ({
-            parametrosDeRelacionamento: '_expand=lead&_expand=servico'
+            parametrosDeRelacionamento: '_expand=lead&_expand=servico',
+            formPesquisa: {
+                id_like: '',
+                data_inicio_gte: '',
+                data_inicio_lte: '',
+            }
         }),
+        methods: {
+            pesquisar() {
+
+                Object.keys(this.formPesquisa).forEach(chave => {
+                    if(this.formPesquisa[chave] == '') delete this.formPesquisa[chave]
+                })
+                const queryParams = new URLSearchParams(this.formPesquisa).toString()
+                const url = `http://127.0.0.1:3000/contratos?${this.parametrosDeRelacionamento}&${queryParams}`
+                this.getDadosApi(url)
+            }
+        },
+
         created() {
-            this.getDadosApi(`http://127.0.0.1:3000/contratos?${this.parametrosDeRelacionamento}`)
+            const queryParams = new URLSearchParams(this.$route.query)
+            const url = `http://127.0.0.1:3000/contratos?${this.parametrosDeRelacionamento}&${queryParams}`
+            this.getDadosApi(url)
         },
         beforeRouteUpdate(to, from, next) {
             const queryParams = new URLSearchParams(to.query).toString()
